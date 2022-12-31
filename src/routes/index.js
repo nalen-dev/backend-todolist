a5b3ab55f251c40125f3591f81d746b252a48a6e
nlndrpraja <nalendrapraja11@gmail.com>
2023-01-01 07:07:43 +0800

    bug: delete task

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,6 @@ router.post("/login", middleware.loginValidation, user.loginHandler);
 router.post("/todos", middleware.authentication, middleware.createTaskValidation, todolist.createTask);
 router.get("/todos", middleware.authentication, todolist.findTasksByUserId);
 router.put("/todos/:id", middleware.authentication, todolist.updateTask);
-router.put("/todos/:id", middleware.authentication, todolist.deleteTask);
+router.delete("/todos/:id", middleware.authentication, todolist.deleteTask);
 
 module.exports = router;
