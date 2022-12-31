fb0bcaa9294401278acb8a6cad198fd5299c9ee9
nlndrpraja <nalendrapraja11@gmail.com>
2023-01-01 01:28:08 +0800

    feat: add middleware folder, and implement login validation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const user = require("../controllers/user");
+const middleware = require("../middleware");
 
-router.post("/login", user.loginHandler);
+router.post("/login", middleware.loginValidation, user.loginHandler);
 
 module.exports = router;
